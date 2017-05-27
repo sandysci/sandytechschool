03857d745265b3e97d40426b1bdc7a779b8b97d5
sandysci <ezeibesandra@gmail.com>
2017-05-27 22:43:03 +0100

    configure node

--- Server.js
+++ Server.js
@@ -2,3 +2,20 @@ const express = require('express');
 const app = express ();
 app.use(express.static(_dirname+'/dist'));
 app.listen (process.env.PORT||8080);
+//force https
+const forceSSL = function(){
+    return function(req,res,next){
+        if(req.headers['x-forwarded-proto'] != 'https'){
+            return res.redirect(
+                ['https://',req.get('Host'),req.url].join('')
+            );
+        }
+        next();
+    }
+}
+//middleware
+app.use(forceSSL());
+const path = require('path');
+app.get('/*',function(req,res){
+    res.sendFile(path.join(_dirname+'/dist/index.html'));
+});
