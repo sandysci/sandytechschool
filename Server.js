a1d6e38cfa5d27f7ce42db9c19ad8eaf204779f3
sandysci <ezeibesandra@gmail.com>
2017-05-30 00:17:19 +0100

    node server2

--- Server.js
+++ Server.js
@@ -1,9 +1,34 @@
+// server.js
 const express = require('express');
-const app = express ();
-app.use(express.static(__dirname+'/dist'));
-app.listen (process.env.PORT||8080,function(){
-    console.log("sandy node"+ process.env.port);
-});
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+// const express = require('express');
+// const app = express ();
+// app.use(express.static(__dirname+'/dist'));
+// app.listen (process.env.PORT||8080,function(){
+//     console.log("sandy node"+ process.env.port);
+// });
 //force https
 // const forceSSL = function(){
 //     return function(req,res,next){
