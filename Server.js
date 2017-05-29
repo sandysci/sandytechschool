648cccc058ef0329245228df64dc3192f2d020b8
sandysci <ezeibesandra@gmail.com>
2017-05-29 23:36:50 +0100

    node server editted

--- Server.js
+++ Server.js
@@ -1,21 +1,31 @@
+// const express = require('express');
+// const app = express ();
+// app.use(express.static(_dirname+'/dist'));
+// app.listen (process.env.PORT||8080);
+// //force https
+// const forceSSL = function(){
+//     return function(req,res,next){
+//         if(req.headers['x-forwarded-proto'] != 'https'){
+//             return res.redirect(
+//                 ['https://',req.get('Host'),req.url].join('')
+//             );
+//         }
+//         next();
+//     }
+// }
+// //middleware
+// app.use(forceSSL());
+// const path = require('path');
+// app.get('/*',function(req,res){
+//     res.sendFile(path.join( __dirname + '/dist'));
+   
+// });
 const express = require('express');
-const app = express ();
-app.use(express.static(_dirname+'/dist'));
-app.listen (process.env.PORT||8080);
-//force https
-const forceSSL = function(){
-    return function(req,res,next){
-        if(req.headers['x-forwarded-proto'] != 'https'){
-            return res.redirect(
-                ['https://',req.get('Host'),req.url].join('')
-            );
-        }
-        next();
-    }
-}
-//middleware
-app.use(forceSSL());
-const path = require('path');
-app.get('/*',function(req,res){
-    res.sendFile(path.join(_dirname+'/dist/index.html'));
-});
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
